4910fbe02fbca8df7bf36191a59a9d11ef8b26f5
Maciel Zeferino <macielzeferino@pop-os.localdomain>
2022-05-24 15:12:06 -0300

    first function of footer flash

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function typeWrite(element) {
+  const textArray = element.innerHTML.split('');
+  element.innerHTML=' ';
+  textArray.forEach(function (letter, i) {
+
+    setTimeout(function () {
+      element.innerHTML += letter;
+    }, 75 * i)
+  });
+}
+const title = document.querySelector('.title-effect');
+typeWrite(title);
